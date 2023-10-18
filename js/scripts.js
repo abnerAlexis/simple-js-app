1a6455618be47327207eb7f2a1b4f63c941c688a
Alexis Abner <alexisabner@alexiss-air.lan>
2023-10-18 17:32:07 -0600

    Update page title, Create object array in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-var pokemonList = [];
+// array of objects
+var pokemonList = [
+    {name: "Bellossom", height: "0.4 m", types: "grass", abilities: ["Chlorophyll", "Healer"]},
+    {name: "Butterfree", height: "1.1 m", types: ["bug", "flying"], abilities: ["Compoundeyes", "Tinted-lens"]},
+    {name: "Rapidash", height: "1.7 m", types: "fire", abilities: ["Flash-fire", "Flame-body", "Run-away"]},
+    {name: "Tentacruel", height: "1.6 m", types: ["water", "poison"], abilities: ["Chlorophyll", "Healer"]},
+    {name: "Moltres", height: "2 m", types: ["fire", "flying"], abilities: ["Pressure", "Flame-body"]}
+];
