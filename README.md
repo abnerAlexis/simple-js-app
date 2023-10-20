a2038d904c1e36951a99dfee19e731135a48cdf9
Alexis Abner <alexisabner@alexiss-air.lan>
2023-10-19 18:04:27 -0600

    Loop through pokemonList conditioned by height

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ window.alert("Hello world!");
 alert("Hello world!");
 ```
 ### variables
-####Example of using var
+### Example of using var
 ```
 var number1 = 45;
 var number2 = 2;
@@ -24,3 +24,30 @@ var multiplied = number1 * number2;
 4. Only use let if you can't use const
 
 5. Only use var if you MUST support old browsers.
+
+
+### [ReferenceError: document is not defined](https://bobbyhadz.com/blog/javascript-referenceerror-document-is-not-defined)
+
+* The "ReferenceError: document is not defined" error occurs for multiple reasons:
+
+1. Using document in Node.js.
+2. Using document on the server (e.g. server-side rendering in Next.js).
+3. Misspelling the document global variable (should be all lowercase).
+
+The code below helps you test whether you are on the server or the browser. 
+
+If you are on the server, you can't use document.write().
+```
+if (typeof window !== 'undefined') {
+  // 👉️ can use document here
+  console.log('You are on the browser')
+
+  console.log(document.title)
+  console.log(document.getElementsByClassName('my-class'));
+} else {
+  // 👉️ can't use document here
+  console.log('You are on the server')
+}
+```
+* Because of this issue, I couldn't use document.write(). It looks like I'm on the server.
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 // array of objects
 var pokemonList = [
-    {name: "Bellossom", height: "0.4 m", types: "grass", abilities: ["Chlorophyll", "Healer"]},
-    {name: "Butterfree", height: "1.1 m", types: ["bug", "flying"], abilities: ["Compoundeyes", "Tinted-lens"]},
-    {name: "Rapidash", height: "1.7 m", types: "fire", abilities: ["Flash-fire", "Flame-body", "Run-away"]},
-    {name: "Tentacruel", height: "1.6 m", types: ["water", "poison"], abilities: ["Chlorophyll", "Healer"]},
-    {name: "Moltres", height: "2 m", types: ["fire", "flying"], abilities: ["Pressure", "Flame-body"]}
+    {name: "Bellossom", height: 0.4, types: "grass", abilities: ["Chlorophyll", "Healer"]},
+    {name: "Butterfree", height: 1.1, types: ["bug", "flying"], abilities: ["Compoundeyes", "Tinted-lens"]},
+    {name: "Rapidash", height: 1.7, types: "fire", abilities: ["Flash-fire", "Flame-body", "Run-away"]},
+    {name: "Tentacruel", height: 1.6, types: ["water", "poison"], abilities: ["Chlorophyll", "Healer"]},
+    {name: "Moltres", height: 2, types: ["fire", "flying"], abilities: ["Pressure", "Flame-body"]},
+    {name: "Rattata", height: 0.3, types: "normal", abilities: ["Run-away", "Hustle", "Guts"]}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 0.5) {
+        document.write(pokemonList[i].name + "'s height is " + pokemonList[i].height + " m. It's a small pokemon.\n");
+    }
+    else if (pokemonList[i].height >= 0.5 && pokemonList[i].height <= 1.5) {
+        document.write(pokemonList[i].name + "'s height is " + pokemonList[i].height + " m. It's an average sized pokemon.\n");
+        
+    } else {
+        document.write(pokemonList[i].name + "'s height is " + pokemonList[i].height + " m. It's a large pokemon.\n");
+    }   
+}
+
