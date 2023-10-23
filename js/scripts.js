4c15b61028c44e0df8c571f2f0c2c8ad504880a6
Alexis Abner <alexisabner@alexiss-air.lan>
2023-10-23 13:18:56 -0600

    Update iteration forEach() instead of for loop

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,15 @@ var pokemonList = [
     {name: "Rattata", height: 0.3, types: "normal", abilities: ["Run-away", "Hustle", "Guts"]}
 ];
 
-//Classifying pokemons by height by looping through the array pokemonList
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height < 0.5) {
-        document.write(pokemonList[i].name + "'s height is " + pokemonList[i].height + " m. It's a small pokemon.");
-    }
-    else if (pokemonList[i].height >= 0.5 && pokemonList[i].height <= 1.5) {
-        document.write(pokemonList[i].name + "'s height is " + pokemonList[i].height + " m. It's an average sized pokemon.");
-        
-    } else {
-        document.write(pokemonList[i].name + "'s height is " + pokemonList[i].height + " m. It's a large pokemon.");
-    }    
-}
+//Classifying pokemons by height by looping through the array pokemonList with forEach()
 
+pokemonList.forEach(pokemon => {
+    if (pokemon.height < 0.5) {
+        document.write('<p>' + pokemon.name + "'s height is " + pokemon.height + " m. It's a small pokemon.</p>");
+    }
+    else if (pokemon.height >= 0.5 && pokemon.height <= 1.5) {
+        document.write("<p>" + pokemon.name + "'s heigth is " + pokemon.height + " m. It's an average sized pokemon.</p>");
+    } else {
+        document.write("<p>" + pokemon.name + "'s height is " + pokemon.height + " m. It's a large pokemon.</p>");
+    }
+});
