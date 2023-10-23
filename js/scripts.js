5d75712fd27ece83456418922f28e092b98340db
Alexis Abner <alexisabner@alexiss-air.lan>
2023-10-23 15:10:23 -0600

    Reorganize code using iife

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
-// array of objects
-var pokemonList = [
-    {name: "Bellossom", height: 0.4, types: "grass", abilities: ["Chlorophyll", "Healer"]},
-    {name: "Butterfree", height: 1.1, types: ["bug", "flying"], abilities: ["Compoundeyes", "Tinted-lens"]},
-    {name: "Rapidash", height: 1.7, types: "fire", abilities: ["Flash-fire", "Flame-body", "Run-away"]},
-    {name: "Tentacruel", height: 1.6, types: ["water", "poison"], abilities: ["Chlorophyll", "Healer"]},
-    {name: "Moltres", height: 2, types: ["fire", "flying"], abilities: ["Pressure", "Flame-body"]},
-    {name: "Rattata", height: 0.3, types: "normal", abilities: ["Run-away", "Hustle", "Guts"]}
-];
+//IIEF    
+let pokemons = (function () {
+    // array of objects
+    var pokemonList = [
+        { name: "Bellossom", height: 0.4, types: "grass", abilities: ["Chlorophyll", "Healer"] },
+        { name: "Butterfree", height: 1.1, types: ["bug", "flying"], abilities: ["Compoundeyes", "Tinted-lens"] },
+        { name: "Rapidash", height: 1.7, types: "fire", abilities: ["Flash-fire", "Flame-body", "Run-away"] },
+        { name: "Tentacruel", height: 1.6, types: ["water", "poison"], abilities: ["Chlorophyll", "Healer"] },
+        { name: "Moltres", height: 2, types: ["fire", "flying"], abilities: ["Pressure", "Flame-body"] },
+        { name: "Rattata", height: 0.3, types: "normal", abilities: ["Run-away", "Hustle", "Guts"] }
+    ];
 
-//Classifying pokemons by height by looping through the array pokemonList with forEach()
-
-pokemonList.forEach(pokemon => {
-    if (pokemon.height < 0.5) {
-        document.write('<p>' + pokemon.name + "'s height is " + pokemon.height + " m. It's a small pokemon.</p>");
+    //Categorizing size of pokemons
+    function displayPokemonbySize(pokemon) {
+        let pokemonSize = '';
+        if (pokemon.height < 0.5) {
+            pokemonSize = 'small';
+        }
+        else if (pokemon.height <= 1.5) {
+            pokemonSize = "average";
+        } else {
+            pokemonSize = 'large';
+        }
+        document.write(`<p>${pokemon.name} is ${pokemon.height} m. It is a ${pokemonSize} pokemon.</p><br>`);
     }
-    else if (pokemon.height >= 0.5 && pokemon.height <= 1.5) {
-        document.write("<p>" + pokemon.name + "'s heigth is " + pokemon.height + " m. It's an average sized pokemon.</p>");
-    } else {
-        document.write("<p>" + pokemon.name + "'s height is " + pokemon.height + " m. It's a large pokemon.</p>");
-    }
-});
+    //Returning the iteration of pokemonList. 
+    return {
+        internalMethod: function () {
+            pokemonList.forEach(displayPokemonbySize);
+        }
+    };
+})();
+//Internal method call
+pokemons.internalMethod();
