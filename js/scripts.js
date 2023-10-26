08fcfc975f02c384a157b0f3a21a1e96487d4d8d
Alexis Abner <alexisabner@alexiss-air.lan>
2023-10-25 20:16:28 -0600

    Use createElement, appendChild etc

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,57 @@
-//IIFE    
-let pokemons = (function () {
-    // array of objects
-    var pokemonList = [
+let pokemonRepository = (function () {
+    let repository = [
         { name: "Bellossom", height: 0.4, types: "grass", abilities: ["Chlorophyll", "Healer"] },
         { name: "Butterfree", height: 1.1, types: ["bug", "flying"], abilities: ["Compoundeyes", "Tinted-lens"] },
         { name: "Rapidash", height: 1.7, types: "fire", abilities: ["Flash-fire", "Flame-body", "Run-away"] },
         { name: "Tentacruel", height: 1.6, types: ["water", "poison"], abilities: ["Chlorophyll", "Healer"] },
         { name: "Moltres", height: 2, types: ["fire", "flying"], abilities: ["Pressure", "Flame-body"] },
-        { name: "Rattata", height: 0.3, types: "normal", abilities: ["Run-away", "Hustle", "Guts"] }
+        { name: "Rattata", height: 0.3, types: "normal", abilities: ["Run-away", "Hustle", "Guts"] },
     ];
 
-    //Categorizing size of pokemons
-    function displayPokemonbySize(pokemon) {
-        let pokemonSize = '';
-        if (pokemon.height < 0.5) {
-            pokemonSize = 'small';
-        }
-        else if (pokemon.height <= 1.5) {
-            pokemonSize = "average";
-        } else {
-            pokemonSize = 'large';
-        }
-        document.write(`<p>${pokemon.name} is ${pokemon.height} m. It is a ${pokemonSize} pokemon.</p><br>`);
-    }
-
-    //Get all pokemon
-    function getAll() {
-        return pokemonList;
-    }
-
-    //Add new pokemon
     function add(pokemon) {
-        pokemonList.push(pokemon);
+        if (
+            typeof pokemon === "object" &&
+            "name" in pokemon &&
+            "height" in pokemon &&
+            "types" in pokemon
+        ) {
+            repository.push(pokemon);
+        } else {
+            console.log("pokemon is not correct");
+        }
     }
-    //Returning the iteration of pokemonList. 
+    function getAll() {
+        return repository;
+    }
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+    }
     return {
-        internalMethod: function () {
-            pokemonList.forEach(displayPokemonbySize);
-        },
-        getAll: getAll,
         add: add,
+        getAll: getAll,
+        addListItem: addListItem
     };
 })();
 
-//Add new pokemon
-pokemons.add({
+pokemonRepository.add({
     name: "Pikachu",
     height: 0.4,
     types: "electric",
     abilities: ["Static", "Lightning Rod"],
   });
 
-//Internal method call
-pokemons.internalMethod();
-document.write(pokemons.getAll());
+pokemonRepository.getAll().forEach(function (pokemon) {
+  let pokemonList = document.querySelector('.pokemon-list');
+  let listPokemon = document.createElement('li');
+  let button = document.createElement('button');
+  button.innerText = pokemon.name;
+  button.className = 'button-class';
+  listPokemon.appendChild(button);
+  pokemonList.appendChild(listPokemon);
+});
